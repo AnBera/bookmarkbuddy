3583e4ccef15c7b4c9c52efd9343f48a8ae6c728
Anirban Bera <anirban.bera.89@gmail.com>
2019-05-11 01:06:11 +0530

    integrate semantic-ui

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
       Bookmarkbuddy React
+      <br/>
+      <Button icon="smile" content="Test semantic Ui"/>
     </div>
   );
 }
