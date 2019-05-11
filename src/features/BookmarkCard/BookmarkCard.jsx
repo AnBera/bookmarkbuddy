2373306094571ce7e02279bf6e5e6d38adc92e24
Anirban Bera <anirban.bera.89@gmail.com>
2019-05-11 13:48:37 +0530

    otutof the box semantic card UI integration

--- src/features/BookmarkCard/BookmarkCard.jsx
+++ src/features/BookmarkCard/BookmarkCard.jsx
@@ -1,28 +1,37 @@
 import React, { Component } from "react";
-import { Grid, Segment } from 'semantic-ui-react';
+import { Grid, Button, Card, Image } from "semantic-ui-react";
 
 class BookmarkCard extends Component {
   render() {
     return (
       <>
         <Grid.Column>
-          <Segment>Content...</Segment>
+          <Card>
+            <Card.Content>
+              <Image
+                floated="right"
+                size="mini"
+                src="https://react.semantic-ui.com/images/avatar/large/steve.jpg"
+              />
+              <Card.Header>Steve Sanders</Card.Header>
+              <Card.Meta>Friends of Elliot</Card.Meta>
+              <Card.Description>
+                Steve wants to add you to the group{" "}
+                <strong>best friends</strong>
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className="ui two buttons">
+                <Button basic color="green">
+                  Approve
+                </Button>
+                <Button basic color="red">
+                  Decline
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
         </Grid.Column>
-        {/* <Grid.Column>
-          <Segment>Content</Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>Content</Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>Content</Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>Content</Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>Content</Segment>
-        </Grid.Column> */}
       </>
     );
   }
