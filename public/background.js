2466ef7652849878917d5885e6cd0fea0bdad783
Anirban Bera <anirban.bera.89@gmail.com>
2019-09-11 00:57:03 +0530

    boilerplate code for popup to background script communication

---
+++ public/background.js
@@ -0,0 +1,9 @@
+/*global chrome*/
+
+chrome.extension.onConnect.addListener(function(port) {
+    console.log("Connected .....");
+    port.onMessage.addListener(function(msg) {
+         console.log("message recieved" + msg);
+         port.postMessage("Hi Popup.js");
+    });
+});
