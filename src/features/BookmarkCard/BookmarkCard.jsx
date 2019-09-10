f0033519d652b226aba84b58142e8bd212e13f94
Animesh Lohar <animeshl913@IN.nam.ad.pwcinternal.com>
2019-09-11 01:39:21 +0530

    Implemented Bookmark Sharing option

--- src/features/BookmarkCard/BookmarkCard.jsx
+++ src/features/BookmarkCard/BookmarkCard.jsx
@@ -4,20 +4,30 @@ import { Card, Image, Label, Icon } from "semantic-ui-react";
 import { extractHostname, generateImageName } from "../../app/common/util/Util";
 import Hover from "../../app/common/Component/Hover";
 import Configs from "../../app/common/constants";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  LinkedinIcon
+} from "react-share";
+import RemoveBookmark from "./DeleteBookmark";
+import EditBookmark from "../EditBookmark/EditBookmark";
 
-import RemoveBookmark from "./DeleteBookmark"
-
-import EditBookmark from '../EditBookmark/EditBookmark'
 class BookmarkCard extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       isEdit: false,
-      selectedFolder:null };
+      selectedFolder: null
+    };
   }
 
   //Changed Bookmark Node Array
-  changedBookamrkFolder=[];
+  changedBookamrkFolder = [];
 
   //selectedFolder={};
   onCategoryClick = e => {
@@ -38,12 +48,14 @@ class BookmarkCard extends Component {
     // e.target.src = 'some default image url'
   };
 
-  closeEditModal=()=>{    
-    this.changedBookamrkFolder.forEach((item)=>{item.isOpen=false;item.isSelected=false});
+  closeEditModal = () => {
+    this.changedBookamrkFolder.forEach(item => {
+      item.isOpen = false;
+      item.isSelected = false;
+    });
     this.props.getUpdateBookmarkTree();
-    this.setState({ isEdit: false },()=>{});
-  }
-  
+    this.setState({ isEdit: false }, () => {});
+  };
 
   render() {
     const { bookmark, colorsMap } = this.props;
@@ -57,17 +69,17 @@ class BookmarkCard extends Component {
         {!this.state.isEdit && (
           <Card fluid>
             <Card.Content href={bookmark.url}>
-              <span className="ui transparent floating label context-icons">
-                <Icon name="pin" size='large' />
+              <span className="ui transparent floating label context-icons">              
+                <Icon name="pin" size="large" />
                 <Icon
                   onClick={e => {
                     e.preventDefault();
                     this.setState({ isEdit: !this.state.isEdit });
                   }}
                   name="edit"
-                  size='large'
+                  size="large"
                 />
-                <RemoveBookmark Objbookmark={bookmark}/>
+                <RemoveBookmark Objbookmark={bookmark} />
               </span>
 
               {/* <Label as='a' color='grey' ribbon='right'>
@@ -111,8 +123,9 @@ class BookmarkCard extends Component {
                   className="padding-right-medium"
                   src={`chrome://favicon/${bookmark.Url}`}
                 />
-                { (hostName.length > 23) ? 
-                ((hostName.substring(0,23-3)) + '...') : hostName}
+                {hostName.length > 23
+                  ? hostName.substring(0, 23 - 3) + "..."
+                  : hostName}
               </div>
               <Card.Meta>{bookmark.title}</Card.Meta>
               <Hover
@@ -132,11 +145,38 @@ class BookmarkCard extends Component {
                   <span className="category" style={style} />
                 </Label>
               </Hover>
+              <FacebookShareButton                
+                url={bookmark.Url}
+                quote={`Shared via Bookmarkbuddy:${bookmark.title}`}
+              ><FacebookIcon round={true} size={"1.5rem"}></FacebookIcon></FacebookShareButton>
+
+              <TwitterShareButton
+                round={true}
+                url={bookmark.Url}
+                title={`Shared via Bookmarkbuddy:${bookmark.title}`}
+              ><TwitterIcon round={true} size={"1.5rem"}></TwitterIcon></TwitterShareButton>
+              <LinkedinShareButton
+                round={true}
+                url={bookmark.Url}
+                title={`Shared via Bookmarkbuddy:${bookmark.title}`}
+              ><LinkedinIcon round={true} size={"1.5rem"}></LinkedinIcon></LinkedinShareButton>
+              <WhatsappShareButton
+                round={true}
+                url={bookmark.Url}
+                title={`Shared via Bookmarkbuddy:${bookmark.title}`}
+              ><WhatsappIcon round={true} size={"1.5rem"}></WhatsappIcon></WhatsappShareButton>
             </Card.Content>
           </Card>
         )}
-        <EditBookmark changedBookamrkFolder={this.changedBookamrkFolder} bookmarkFolderTree={this.props.bookmarkFolderTree} updateBookamark={this.props.updateBookamark} 
-        colorsMap={colorsMap} selectedBookmark={bookmark} isOpen={this.state.isEdit} closeModal={this.closeEditModal} />
+        <EditBookmark
+          changedBookamrkFolder={this.changedBookamrkFolder}
+          bookmarkFolderTree={this.props.bookmarkFolderTree}
+          updateBookamark={this.props.updateBookamark}
+          colorsMap={colorsMap}
+          selectedBookmark={bookmark}
+          isOpen={this.state.isEdit}
+          closeModal={this.closeEditModal}
+        />
       </>
     );
   }
