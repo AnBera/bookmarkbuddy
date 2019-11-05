d1796a2bb3348fb900138089327b2c192b983dbd
Animesh Lohar <animeshl913@IN.nam.ad.pwcinternal.com>
2019-11-06 03:13:02 +0530

    Update click count form popup screen and popular bookmark card

--- src/features/BookmarkCard/BookmarkRecommendationCard.jsx
+++ src/features/BookmarkCard/BookmarkRecommendationCard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Card, Image, Header, Icon } from "semantic-ui-react";
 import { extractHostname } from "../../app/common/util/Util";
 import { getPopularBookmarks } from "../../services/PreviewBookmarkService";
-
+import { increaseHitCount } from "../../services/PreviewBookmarkService";
 const BookmarkRecommendationCard = props => {
   const [topBookmarks, settopBookmarks] = useState([]);
   useEffect(() => {
@@ -11,7 +11,13 @@ const BookmarkRecommendationCard = props => {
         settopBookmarks(response.bookmarks);
     });
   }, []);
-
+  const updateHitCount = (url, shardKey) => {
+    increaseHitCount({
+      uniqueID: props.userId,
+      url: url,
+      shardKey: shardKey
+    });
+  };
   return (
     <div className="recommendation-card-container">
       <Header as="h4" className="recommendation-card-header">
@@ -31,8 +37,14 @@ const BookmarkRecommendationCard = props => {
             className="recommendation-card"
             href={bookmark.url}
             key={bookmark.id}
+            onClick={e =>
+              updateHitCount(
+                bookmark.url,
+                extractHostname(bookmark.url).charAt(0)
+              )
+            }
           >
-            <Card.Content>
+            <Card.Content target="_blank" href={bookmark.url}>
               <div className="url-heading">
                 <Image
                   className="padding-right-medium"
@@ -40,33 +52,42 @@ const BookmarkRecommendationCard = props => {
                 />
                 {extractHostname(bookmark.url)}
               </div>
-              <Card.Meta style={{fontWeight:500}}> {bookmark.title}</Card.Meta>
-              <Card.Meta style={{fontFamily:'Rubik', fontWeight:500}}>
-                <Icon name='chart line' /> {bookmark.hitCount} Clicks
+              <Card.Meta style={{ fontWeight: 500 }}>
+                {" "}
+                {bookmark.title}
+              </Card.Meta>
+              <Card.Meta style={{ fontFamily: "Rubik", fontWeight: 500 }}>
+                <Icon name="chart line" /> {bookmark.hitCount} Clicks
               </Card.Meta>
             </Card.Content>
           </Card>
         ))}
 
-      {topBookmarks &&
-        topBookmarks.length === 0 &&  
-        (<div style={{color:'#a9a9a9', padding:'1em'}}>
-          Your popular bookmark data is being generated. Meanwhile here are some bookmarks you might be interested in.
-        </div>)}
+      {topBookmarks && topBookmarks.length === 0 && (
+        <div style={{ color: "#a9a9a9", padding: "1em" }}>
+          Your popular bookmark data is being generated. Meanwhile here are some
+          bookmarks you might be interested in.
+        </div>
+      )}
 
       {topBookmarks &&
         topBookmarks.length === 0 &&
         props.bookmarks &&
         props.bookmarks.length > 0 &&
         props.bookmarks.map(bookmark => (
-          
           <Card
             fluid
             className="recommendation-card"
             href={bookmark.url}
             key={bookmark.id}
+            onClick={e =>
+              updateHitCount(
+                bookmark.url,
+                extractHostname(bookmark.url).charAt(0)
+              )
+            }
           >
-            <Card.Content>
+            <Card.Content target="_blank" href={bookmark.url}>
               <div className="url-heading">
                 <Image
                   className="padding-right-medium"
@@ -74,7 +95,9 @@ const BookmarkRecommendationCard = props => {
                 />
                 {extractHostname(bookmark.url)}
               </div>
-              <Card.Meta style={{fontWeight:500}}>{bookmark.title}</Card.Meta>
+              <Card.Meta style={{ fontWeight: 500 }}>
+                {bookmark.title}
+              </Card.Meta>
             </Card.Content>
           </Card>
         ))}
--- src/features/PopupContainer.jsx
+++ src/features/PopupContainer.jsx
@@ -3,46 +3,81 @@ import React, { Component } from "react";
 import BookmarkbuddyLogoGrey3 from "./../app/assets/images/BookmarkbuddyLogoGrey3.png";
 import { Card, Image, Button, Icon } from "semantic-ui-react";
 import SearchAndFilter from "./Search/SearchandFilter";
-import { extractHostname,filterList,debounce } from "../app/common/util/Util";
-
+import { extractHostname, filterList, debounce } from "../app/common/util/Util";
+import { increaseHitCount } from "../services/PreviewBookmarkService";
 
 class PopupContainer extends Component {
   constructor(props) {
     super(props);
-  this.state={
-    flattenedBookmarks:[],
-    filteredBookmarks:[],
-    bookmarkFolders:[],
-    selectedFolder:'',
-    searchedText:'',
-    isDropDownOpen:false,
-    BookmarksInState:[]
-  };
-  // this.popupcardcontainerref = React.createRef();
-}
- localBookmarks=[];
+    this.state = {
+      flattenedBookmarks: [],
+      filteredBookmarks: [],
+      bookmarkFolders: [],
+      selectedFolder: "",
+      searchedText: "",
+      isDropDownOpen: false,
+      BookmarksInState: [],
+      userId: ""
+    };
+    // this.popupcardcontainerref = React.createRef();
+  }
+  localBookmarks = [];
 
-componentDidMount(){
-  var that=this;
+  componentWillMount() {
+    this.setUserID();
+  }
+  componentDidMount() {
+    var that = this;
     var port = chrome.extension.connect({
       name: "Sample Communication"
     });
     port.postMessage("Hi BackGround");
     port.onMessage.addListener(function(msg) {
-      that.setState({flattenedBookmarks: msg},()=>
-      {console.log("Bookmarks recieved \n" + JSON.stringify(that.state.flattenedBookmarks))});
-    });  
-    // this.setState({flattenedBookmarks: books},()=>
-    // {console.log("Bookmarks recieved \n" + JSON.stringify(this.state.flattenedBookmarks))});  
+      that.setState({ flattenedBookmarks: msg }, () => {
+        console.log(
+          "Bookmarks recieved \n" +
+            JSON.stringify(that.state.flattenedBookmarks)
+        );
+      });
+    });
   }
+  setUserID = () => {
+    chrome.storage.sync.get(["uniqueID"], items => {
+      if (Object.keys(items).length === 0 && items.constructor === Object) {
+        let userid = this.getRandomToken();
+        chrome.storage.sync.set({ uniqueID: userid }, () => {
+          this.setState({ userId: items.uniqueID });
+        });
+      } else {
+        this.setState({ userId: items.uniqueID });
+      }
+    });
+  };
 
+  getRandomToken = () => {
+    let randomPool = new Uint8Array(32);
+    crypto.getRandomValues(randomPool);
+    let hex = "";
+    for (var i = 0; i < randomPool.length; ++i) {
+      hex += randomPool[i].toString(16);
+    }
+    console.log(hex);
+    return hex;
+  };
+  updateHitCount = (url, shardKey) => {
+    increaseHitCount({
+      uniqueID: this.state.userId,
+      url: url,
+      shardKey: shardKey
+    });
+  };
   handleContextRef = ref => {
     console.log(ref);
-    if(ref) {
+    if (ref) {
       console.log(ref.current);
 
       ref.onscroll = debounce(() => {
-        console.log('scrolling happening!')
+        console.log("scrolling happening!");
         let scrollTop = ref.scrollTop;
         let windowHeight = ref.clientHeight; //ref.innerHeight;
         let bodyHeight = ref.scrollHeight - windowHeight;
@@ -59,80 +94,76 @@ componentDidMount(){
     }
   };
 
-  componentWillUpdate(newProps,newState){
-    if(this.state.flattenedBookmarks.length===0 && newState.flattenedBookmarks.length>0)
-    {this.localBookmarks.push(...newState.flattenedBookmarks);
+  componentWillUpdate(newProps, newState) {
+    if (
+      this.state.flattenedBookmarks.length === 0 &&
+      newState.flattenedBookmarks.length > 0
+    ) {
+      this.localBookmarks.push(...newState.flattenedBookmarks);
       this.addBookmarksInState(18);
-      // // this.popupcardcontainerref.addEventListener('scroll', () => console.log('scroll!'));
-      
-      // window.onscroll = debounce(() => {
-      //   let scrollTop = document.documentElement.scrollTop;
-      //   let windowHeight = window.innerHeight;
-      //   let bodyHeight = document.documentElement.scrollHeight - windowHeight;
-      //   let scrollPercentage = scrollTop / bodyHeight;
-
-      //   console.log(scrollTop, windowHeight, bodyHeight, scrollPercentage);
-
-      //   // if the scroll is more than 70% from the top, load more content.
-      //   if (scrollPercentage > 0.7) {
-      //     // Load more content!
-      //     this.addBookmarksInState(21);
-      //   }
-      // }, 100);  
-    if (newState.flattenedBookmarks.length > 0) {
-      let folders = [];
-      newState.flattenedBookmarks.map(name => {
-        if (!folders.find(item => item.key === name.category)) {
-          folders.push({
-            key: name.category,
-            text: name.category,
-            value: name.category,
-          });
-        }
-      });
-      folders.sort();
-      folders.unshift({
-        key: "-- Select all --",
-        text:"-- Select all --",
-        value:"-- Select all --",
-      });
-      this.setState({bookmarkFolders:folders});
-  };
-}
+      if (newState.flattenedBookmarks.length > 0) {
+        let folders = [];
+        newState.flattenedBookmarks.map(name => {
+          if (!folders.find(item => item.key === name.category)) {
+            folders.push({
+              key: name.category,
+              text: name.category,
+              value: name.category
+            });
+          }
+        });
+        folders.sort();
+        folders.unshift({
+          key: "-- Select all --",
+          text: "-- Select all --",
+          value: "-- Select all --"
+        });
+        this.setState({ bookmarkFolders: folders });
+      }
+    }
   }
 
   addBookmarksInState = numberOfBookmarks => {
     setTimeout(() => {
       if (
-       this.state.BookmarksInState.length + numberOfBookmarks <
+        this.state.BookmarksInState.length + numberOfBookmarks <
         this.localBookmarks.length
-      ) {        
-        this.setState({BookmarksInState:this.localBookmarks.slice(0,this.state.BookmarksInState.length + numberOfBookmarks)});
+      ) {
+        this.setState({
+          BookmarksInState: this.localBookmarks.slice(
+            0,
+            this.state.BookmarksInState.length + numberOfBookmarks
+          )
+        });
       } else if (
         this.localBookmarks.length - this.state.BookmarksInState.length > 0 &&
         this.localBookmarks.length - this.state.BookmarksInState.length <=
           numberOfBookmarks
       ) {
-        this.setState({BookmarksInState:this.localBookmarks.slice(0)});
+        this.setState({ BookmarksInState: this.localBookmarks.slice(0) });
       }
     }, 0);
   };
 
-  open_CloseDropdown=()=>{
-    this.setState({isDropDownOpen:!this.state.isDropDownOpen});
-  }
+  open_CloseDropdown = () => {
+    this.setState({ isDropDownOpen: !this.state.isDropDownOpen });
+  };
 
   searchBookmarkWithinFolder = (searchedText, selectedFolder) => {
-    this.setState({searchedText:searchedText});
-    this.setState({selectedFolder:selectedFolder});
+    this.setState({ searchedText: searchedText });
+    this.setState({ selectedFolder: selectedFolder });
     let filteredBookmarks = [];
     //no folder selected
     if (selectedFolder === "-- Select all --" || selectedFolder === "") {
       //no folder selected no searchtext selected
-      if (searchedText === "") filteredBookmarks = [...this.state.flattenedBookmarks];
+      if (searchedText === "")
+        filteredBookmarks = [...this.state.flattenedBookmarks];
       //no folder selected some searchtext selected
       else
-        filteredBookmarks = filterList(searchedText, this.state.flattenedBookmarks);
+        filteredBookmarks = filterList(
+          searchedText,
+          this.state.flattenedBookmarks
+        );
       //some folder selected
     } else {
       //some folder selected no searchtext selected
@@ -143,7 +174,7 @@ componentDidMount(){
       }
       //some folder selected some searchtext selected
       else
-       filteredBookmarks = filterList(
+        filteredBookmarks = filterList(
           searchedText,
           this.state.flattenedBookmarks.filter(
             element => element.category === selectedFolder
@@ -151,76 +182,87 @@ componentDidMount(){
         );
     }
     this.localBookmarks = [];
-    if(selectedFolder === "-- Select all --" && searchedText === "")
-    this.localBookmarks.push(...this.state.flattenedBookmarks);
-    else
-    this.localBookmarks.push(...filteredBookmarks);
+    if (selectedFolder === "-- Select all --" && searchedText === "")
+      this.localBookmarks.push(...this.state.flattenedBookmarks);
+    else this.localBookmarks.push(...filteredBookmarks);
     this.setState({ BookmarksInState: [] });
     this.addBookmarksInState(15);
   };
 
-  render(){
-  return (
-    <>
-      <div
-        style={{
-          backgroundColor: "#161626",
-          textAlign: "center",
-          height: "60px",
-          paddingTop: "1em"
-        }}
-      >
-        <img src={BookmarkbuddyLogoGrey3} style={{width: "70%"}} alt="BookmarkBuddy" />
-      </div>
-      <div style={{padding: "0 1em"}}>
-        <SearchAndFilter
-          optionList={this.state.bookmarkFolders}
-          setSearchedText={this.searchBookmarkWithinFolder}
-          setSelectedFolder={this.searchBookmarkWithinFolder}
-          open_CloseDropdown={this.open_CloseDropdown}
-          SearchedText={this.state.searchedText}
-          SelectedFolder={this.state.selectedFolder}
-          IsDropDownOpen={this.state.isDropDownOpen}
-        />
-      </div>
-      {this.state.BookmarksInState.length > 0 && (
-        <div className="popup-card-container" ref={this.handleContextRef} >
-          {this.state.BookmarksInState.map((bookmark, i) => (
-            <Card
-              fluid
-              className="recommendation-card"
-              href={bookmark.url}
-              key={bookmark.id}
-              target="_blank"
-            >
-              <Card.Content>
-                <div className="url-heading">
-                  <Image
-                    className="padding-right-medium"
-                    src={`chrome://favicon/${bookmark.url}`}
-                  />
-                  {extractHostname(bookmark.url)}
-                </div>
-                <Card.Meta>{bookmark.title}</Card.Meta>
-              </Card.Content>
-            </Card>
-          ))}
+  render() {
+    return (
+      <>
+        <div
+          style={{
+            backgroundColor: "#161626",
+            textAlign: "center",
+            height: "60px",
+            paddingTop: "1em"
+          }}
+        >
+          <img
+            src={BookmarkbuddyLogoGrey3}
+            style={{ width: "70%" }}
+            alt="BookmarkBuddy"
+          />
         </div>
-      )}
-      <div style={{
-        position: "fixed",
-        bottom: "0",
-        backgroundColor: "#282c34",
-        width: "100%",
-        padding: "1em"
-      }}>
-        <Button Id="openFull" basic color="green" inverted>
-          <Icon name="external alternate" /> Open Full view
-        </Button>
-      </div>
-    </>
-  );
-}
+        <div style={{ padding: "0 1em" }}>
+          <SearchAndFilter
+            optionList={this.state.bookmarkFolders}
+            setSearchedText={this.searchBookmarkWithinFolder}
+            setSelectedFolder={this.searchBookmarkWithinFolder}
+            open_CloseDropdown={this.open_CloseDropdown}
+            SearchedText={this.state.searchedText}
+            SelectedFolder={this.state.selectedFolder}
+            IsDropDownOpen={this.state.isDropDownOpen}
+          />
+        </div>
+        {this.state.BookmarksInState.length > 0 && (
+          <div className="popup-card-container" ref={this.handleContextRef}>
+            {this.state.BookmarksInState.map((bookmark, i) => (
+              <Card
+                fluid
+                onClick={e =>
+                  this.updateHitCount(
+                    bookmark.url,
+                    extractHostname(bookmark.url).charAt(0)
+                  )
+                }
+                className="recommendation-card"
+                href={bookmark.url}
+                key={bookmark.id}
+                target="_blank"
+              >
+                <Card.Content target="_blank" href={bookmark.url}>
+                  <div className="url-heading">
+                    <Image
+                      className="padding-right-medium"
+                      src={`chrome://favicon/${bookmark.url}`}
+                    />
+                    {extractHostname(bookmark.url)}
+                  </div>
+                  <Card.Meta>{bookmark.title}</Card.Meta>
+                </Card.Content>
+              </Card>
+            ))}
+          </div>
+        )}
+        <div
+          style={{
+            position: "fixed",
+            bottom: "0",
+            backgroundColor: "#282c34",
+            width: "100%",
+            padding: "1em"
+          }}
+        >
+          <Button Id="openFull" basic color="green" inverted>
+            <Icon name="external alternate" /> Open Full view
+          </Button>
+        </div>
+      </>
+    );
+  }
 }
 
 export default PopupContainer;
